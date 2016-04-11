571fff46dfbd48a719509dbe8d516a81d858121a
Heley Chen <zccz14@outlook.com>
2016-04-11 14:14:28 +0800

    配置Session

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/room-application');
 var app = express();
+var session = require('express-session');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'SECRET_KEY',
+    key: 'SessionID',
+    resave: false,
+    saveUninitialized: true,
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/applicant',require('./routes/applicant'));
 
 module.exports = app;
