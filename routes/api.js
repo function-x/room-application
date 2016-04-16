283b0a5d37a014052678031993a449fa67233860
Heley Chen <zccz14@outlook.com>
2016-04-16 16:06:04 +0800

    新增API：添加第一个管理员

--- routes/api.js
+++ routes/api.js
@@ -94,4 +94,51 @@ module.exports = require('express').Router()
                 });
             }
         });
-    });
+    })
+    // when there is no administrator, create one
+    .post('/init', function(req, res, next) {
+        User.find({
+            group: 'administrator',
+        }, function(err, users) {
+            if (err) {
+                console.log('init failed', err);
+                res.json({
+                    code: -1,
+                    msg: 'err',
+                    body: {}
+                });
+            } else {
+                if (users.length == 0) {
+                    next(); // has no admin
+                } else {
+                    res.json({
+                        code: 1,
+                        msg: 'has admin',
+                        body: {}
+                    });
+                }
+            }
+        });
+    })
+    .post('/init', function(req, res, next) {
+        new User({
+            username: req.body.username,
+            password: req.body.password,
+            group: 'administrator'
+        }).save(function(err, user) {
+            if (err) {
+                console.log('create admin failed', err);
+                res.json({
+                    code: -1,
+                    msg: 'err',
+                    body: {}
+                });
+            } else {
+                res.json({
+                    code: 0,
+                    msg: 'ok',
+                    body: {}
+                });
+            }
+        });
+    })
